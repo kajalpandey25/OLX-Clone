60aa10423a6625ad9f4d1b0308a396533746cf32
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-12-19 11:49:57 +0530

    Ok

--- node-app/index.js
+++ node-app/index.js
@@ -1,1 +1,13 @@
-console.log("Kajal");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
