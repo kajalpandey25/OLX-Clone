f3c06b238edd425a91d7c80db447285060512694
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-12-19 11:49:22 +0530

    Ok

---
+++ react-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header(){
+    return(
+        <div>
+           <div>
+            SELL & PURCHASE ONLINE...In your City.
+           </div>
+        </div>
+    )
+}
+
+export default Header;
